df65723ed6c0b2dd3f8265f8d535fcf33f13209c
Liam Maddison <maddison.liam@gmail.com>
2019-08-28 20:53:56 +0100

    All of our config modules

---
+++ config/index.js
@@ -0,0 +1,24 @@
+/**
+ * CONFIG/INDEX
+ * 
+ * This is our base configuration file
+ */
+ 
+// intitialize our environment vars
+require("dotenv").config();
+
+const youtube = require("./youtube");
+const database = require("./database");
+
+const config = {
+  youtube,
+  database,
+};
+
+module.exports = (configToReturn = '') => {
+  if (!configToReturn) {
+    return config;
+  }
+
+  return config[configToReturn];
+};
---
+++ config/youtube.js
@@ -0,0 +1,8 @@
+/**
+ * CONFIG/AUTH
+ */
+
+module.exports = {
+  apiKey: process.env.API_KEY,
+  searchFilterFile: `${__dirname}/search_filter.txt`,
+};
