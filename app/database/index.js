ae752a56374af46d2f483fed7917311ad426fab8
Liam Maddison <maddison.liam@gmail.com>
2019-08-28 20:54:43 +0100

    Our base database file

---
+++ app/database/index.js
@@ -0,0 +1,16 @@
+/**
+ * DATABASE/INDEX
+ * 
+ * This is our base database connection file.
+ * Here we require knex, our query builder which
+ *  helps us manage our database queries
+ * We use bookself to manage our data modeling,
+ *  allowing to do great things!
+ */
+
+// initialize knex, our ORM
+const config = require("../../config")("database");
+const knex = require("knex")(config);
+const bookshelf = require("bookshelf")(knex);
+
+module.exports = bookshelf;
